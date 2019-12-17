e42121924b95cd2fc36d942d47dc61073f95e04b
Mike Burnham <mburnham@gci.com>
2019-12-17 14:27:23 -0900

    use new pendingUrl

--- background.js
+++ background.js
@@ -1,18 +1,15 @@
 chrome.tabs.onCreated.addListener(function(newTab) {
-  const checkUrl = 'https://play.google.com/music'
+  const checkUrl = 'https://play.google.com/music';
 
-  if (newTab.url === checkUrl) {
+  if (newTab.pendingUrl === checkUrl) {
     chrome.tabs.getAllInWindow(newTab.windowId, function(tabs) {
       tabs.forEach(function(otherTab) {
-        if (
-          otherTab.id !== newTab.id &&
-          otherTab.url.startsWith(checkUrl)
-        ) {
+        if (otherTab.id !== newTab.id && otherTab.url.startsWith(checkUrl)) {
           chrome.tabs.update(otherTab.id, { selected: true });
           chrome.tabs.remove(newTab.id);
           return;
         }
       });
     });
-  };
+  }
 });
