ca474e832eb4c4a754540a8761089b180952270d
Mike Burnham <mburnham@gci.com>
2020-07-27 16:20:40 -0800

    add support for youtube music

--- background.js
+++ background.js
@@ -1,40 +1,42 @@
 chrome.tabs.onCreated.addListener(function(newTab) {
-  const checkUrl = 'https://play.google.com/music';
+  const checkUrls = ['https://play.google.com/music', 'https://music.youtube.com/'];
 
-  if (newTab.pendingUrl === checkUrl) {
-    console.debug(`pending url: ${newTab.pendingUrl}`);
-    chrome.tabs.query({ url: `${checkUrl}*` }, function(tabs) {
-      console.debug(`tab count: ${tabs.length}`);
+  const checkUrlIndex = checkUrls.indexOf(newTab.pendingUrl);
+  if (checkUrlIndex >= 0) {
+    // console.debug(`pending url: ${newTab.pendingUrl}`);
+    // console.debug(`index: ${checkUrlIndex}`)
+    chrome.tabs.query({ url: `${checkUrls[checkUrlIndex]}*` }, function(tabs) {
+      // console.debug(`tab count: ${tabs.length}`);
 
       if (tabs.length == 1) {
-        console.debug(`new tab id: ${tabs[0].id} window id: ${tabs[0].windowId}`);
+        // console.debug(`new tab id: ${tabs[0].id} window id: ${tabs[0].windowId}`);
         chrome.windows.getAll({ populate: true }, function(windows) {
-          console.debug(`window count: ${windows.length}`);
+          // console.debug(`window count: ${windows.length}`);
 
           let maxTabsWindow = windows[0];
           windows.forEach(value => {
-            console.debug(`window ${value.id} tab count: ${value.tabs.length}`);
+            // console.debug(`window ${value.id} tab count: ${value.tabs.length}`);
             if (value.tabs.length > maxTabsWindow.tabs.length) {
               maxTabsWindow = value;
             }
           });
 
-          console.debug(`moving tab to window id: ${maxTabsWindow.id}`);
+          // console.debug(`moving tab to window id: ${maxTabsWindow.id}`);
           chrome.tabs.move(tabs[0].id, { windowId: maxTabsWindow.id, index: 0 });
-          console.debug(`pinning tab id: ${tabs[0].id}`);
+          // console.debug(`pinning tab id: ${tabs[0].id}`);
           chrome.tabs.update(tabs[0].id, { pinned: true });
-          console.debug(`selecting tab id: ${tabs[0].id}`);
+          // console.debug(`selecting tab id: ${tabs[0].id}`);
           chrome.tabs.update(tabs[0].id, { selected: true });
-          console.debug(`focusing window id: ${tabs[0].windowId}`);
-          chrome.windows.update(tabs[0].windowId, { focused: true });
+          // console.debug(`focusing window id: ${maxTabsWindow.id}`);
+          chrome.windows.update(maxTabsWindow.id, { focused: true });
         });
 
       } else if (tabs.length > 1) {
-        console.debug(`focusing window id: ${tabs[0].windowId}`);
+        // console.debug(`focusing window id: ${tabs[0].windowId}`);
         chrome.windows.update(tabs[0].windowId, { focused: true });
-        console.debug(`selecting tab id: ${tabs[0].id}`);
+        // console.debug(`selecting tab id: ${tabs[0].id}`);
         chrome.tabs.update(tabs[0].id, { selected: true });
-        console.debug(`removing tab id: ${newTab.id}`);
+        // console.debug(`removing tab id: ${newTab.id}`);
         chrome.tabs.remove(newTab.id);
       }
     });
